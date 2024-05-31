39253382b5d820aea7b367069f6da194d7ad4e0b
Alex Goris <alex.goris@conneqtr.com>
2024-05-31 11:19:53 +0200

    added message type to logging

--- SampleService/Filters/TenantMessageFilter.cs
+++ SampleService/Filters/TenantMessageFilter.cs
@@ -20,21 +20,21 @@ public class TenantMessageFilter : IFilter<PublishContext<ITenantMessage>>, IFil
     {
         context.Message.TenantId = Random.Shared.Next();
 
-        _logger.LogInformation("PUBLISH-FILTER: Set tenant id to {tenantId}", context.Message.TenantId);
+        _logger.LogInformation("PUBLISH-FILTER: Processing {messageType}, set tenant id to {tenantId}", context.Message.GetType().Name, context.Message.TenantId);
 
         return next.Send(context);
     }
 
     public Task Send(ConsumeContext<ITenantMessage> context, IPipe<ConsumeContext<ITenantMessage>> next)
     {
-        _logger.LogInformation("CONSUME-FILTER: ITenantMessage with tenant id {tenantId}", context.Message.TenantId);
+        _logger.LogInformation("CONSUME-FILTER: Processing {messageType}, has tenant id {tenantId}", context.Message.GetType().Name, context.Message.TenantId);
 
         return next.Send(context);
     }
 
     public Task Send(ConsumeContext<IsItTime> context, IPipe<ConsumeContext<IsItTime>> next)
     {
-        _logger.LogInformation("CONSUME-FILTER: IsItTime with tenant id {tenantId}", context.Message.TenantId);
+        _logger.LogInformation("CONSUME-FILTER: Processing {messageType}, has tenant id {tenantId}", context.Message.GetType().Name, context.Message.TenantId);
 
         return next.Send(context);
     }
